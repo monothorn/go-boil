0ac471663e2761d7062118443b6819e0e662d151
saurabh-rayakwar <saurabh.rayakwar@edureka.co>
2018-08-27 18:30:10 +0530

    wrapper resty

--- utilities/requestUtility.go
+++ utilities/requestUtility.go
@@ -11,19 +11,60 @@ type RequestUtility interface {
 type requestConfig struct {
 	baseUrl        string
 	requestMethod  string
-	requestParams  string
+	requestParams  map[string]string
 	requestHeaders map[string]string
 }
 
 func (rc *requestConfig) MakeGet(url string) (string, error) {
-	resp, err := resty.R().Get("http://httpbin.org/get")
+	resty.SetDebug(true)
+	resty.SetHeaders(rc.requestHeaders)
+	resty.SetQueryParams(rc.requestParams)
+
+	resp, err := resty.R().Get(rc.baseUrl + url)
 	if err != nil {
 		return "nil", err
 	}
 	return string(resp.Body()), nil
 }
 
-func NewRequestUtility(baseUrl string, requestMethod string, requestParams string, requestHeaders map[string]string) RequestUtility {
+func (rc *requestConfig) MakeDelete(url string) (string, error) {
+	resty.SetDebug(true)
+	resty.SetHeaders(rc.requestHeaders)
+
+	resp, err := resty.R().Delete(rc.baseUrl + url)
+	if err != nil {
+		return "nil", err
+	}
+	return string(resp.Body()), nil
+}
+
+func (rc *requestConfig) MakePost(url string) (string, error) {
+	resty.SetDebug(true)
+	resty.SetHeaders(rc.requestHeaders)
+	resty.SetFormData(rc.requestParams)
+
+	resp, err := resty.R().Post(rc.baseUrl + url)
+
+	if err != nil {
+		return "nil", err
+	}
+	return string(resp.Body()), nil
+}
+
+func (rc *requestConfig) MakePut(url string) (string, error) {
+	resty.SetDebug(true)
+	resty.SetHeaders(rc.requestHeaders)
+	resty.SetFormData(rc.requestParams)
+
+	resp, err := resty.R().Put(rc.baseUrl + url)
+
+	if err != nil {
+		return "nil", err
+	}
+	return string(resp.Body()), nil
+}
+
+func NewRequestUtility(baseUrl string, requestMethod string, requestParams map[string]string, requestHeaders map[string]string) RequestUtility {
 	return &requestConfig{
 		baseUrl, requestMethod, requestParams, requestHeaders,
 	}
